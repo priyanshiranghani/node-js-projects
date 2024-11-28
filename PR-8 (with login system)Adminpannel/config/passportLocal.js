d92fabefe42f1f714986f39e9bae61a4d15f4fad
priyanshiranghani <ranghanipriyanshi402@gmail.com>
2024-11-28 15:57:43 +0530

    project

---
+++ PR-8 (with login system)Adminpannel/config/passportLocal.js
@@ -0,0 +1,52 @@
+const passport = require('passport');
+
+const passportLocal = require('passport-local').Strategy;
+
+const UserModel = require('../models/UserModel');
+
+passport.use(new passportLocal({
+    usernameField : 'email'
+},async(email,password,done)=>{
+    try{
+        const user = await UserModel.findOne({email : email});
+        if(!user || user.password != password){
+            console.log("Email and Password not match");
+            return done(null,false);
+        }
+        return done(null,user)
+    }catch(err){
+        console.log(err);
+        return done(null,false)  
+    }
+}))
+
+passport.serializeUser((user,done)=>{
+    return done(null,user.id);
+})
+
+passport.deserializeUser(async(id,done)=>{
+    try{
+        const user = await UserModel.findById(id);
+        return done(null,user)    
+    }catch(err){
+        console.log(err);
+        return done(null,false);
+    }
+})
+
+
+passport.checkUser = (req,res,next) => {
+        if(!req.isAuthenticated()){
+            return res.redirect('/')
+        }
+        return next();        
+} 
+
+passport.setUser = (req,res,next) => {
+    if(req.isAuthenticated()){
+        res.locals.user = req.user
+    }
+    return next();
+}
+
+module.exports = passport;
---
+++ PR-8 (with login system)Adminpannel/routes/authRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const { loginPage, registerPage, registerUser,loginUser,dashboardPage,logoutUser,forgotPassword,otpPage,postOtp,newpass,postNewpassword } = require('../controllers/AuthController');
+
+const passport = require('passport');
+
+routes.get('/',loginPage)
+routes.get('/register',registerPage)
+routes.post('/registeruser',registerUser);
+routes.post('/loginuser',passport.authenticate('local',{failureRedirect:'/'}),loginUser)
+routes.get('/dashboard',passport.checkUser,dashboardPage);
+routes.get('/logoutuser',logoutUser)
+
+//forgot password
+routes.post('/forgotpassword',forgotPassword)
+routes.get('/otp',otpPage)
+routes.post('/postotp',postOtp)
+routes.get('/newpass',newpass)
+routes.post('/postnewpassword',postNewpassword)
+
+module.exports = routes;
